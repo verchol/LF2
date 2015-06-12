f1fc188d897fbe6209df3e7953447474e8c96210
Spyes <lnitzberg@gmail.com>
2015-06-12 08:14:24 +0300

    setting up environment (consider NOT using angular?)

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.set('port', 3000);
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/public/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+
+app.get('*', function (req, res) {
+  res.render('index.html');
+});
+
+var server = app.listen(3000);
