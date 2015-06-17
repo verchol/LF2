6c73fee260454e5d81795a46113c5a44684acec2
Uri <Urikutner@gmail.com>
2015-06-17 19:16:43 +0300

    refactored presentation.js

--- public/js/presentation.js
+++ public/js/presentation.js
@@ -6,25 +6,29 @@ function presentation() {
   var DOMcanvas = canvas.DOMElement;
   var ctx = DOMcanvas.getContext("2d");
 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  arenaTopLeftX = 0;
-  arenaTopLeftY = canvas.height - arena.height;
-  /*
-  console.log("top left Y: " + arenaTopLeftY);
-  console.log("canvas height: " + canvas.height);
-  console.log("arena height: " + arena.height);
-  arenaStartWidth = canvas.height - arena.height;
-  arenaStartHeight = canvas.width - arena.width;
-  */
-  ctx.fillStyle = "#FF0000";
-  ctx.fillRect(arenaTopLeftX, arenaTopLeftY, arena.width, arena.height);
-  ctx.fillStyle = "#0000FF";
-  ctx.fillRect(0, 0, arena.width, arenaTopLeftY);
+  //requestAnimationFrame(presentation);
   
-  ctx.fillStyle = "#00FFFF";
-  game.players.forEach(function (p) {
-    ctx.fillRect(p.x, p.y, p.size, p.size);
-  });
-
-  requestAnimationFrame(presentation);
+  setInterval(drawPage, 50);
+  
+  function drawPage(){
+	  ctx.clearRect(0, 0, canvas.width, canvas.height);
+	  arenaTopLeftX = 0;
+	  arenaTopLeftY = canvas.height - arena.height;
+	  /*
+	  console.log("top left Y: " + arenaTopLeftY);
+	  console.log("canvas height: " + canvas.height);
+	  console.log("arena height: " + arena.height);
+	  arenaStartWidth = canvas.height - arena.height;
+	  arenaStartHeight = canvas.width - arena.width;
+	  */
+	  ctx.fillStyle = "#FF0000";
+	  ctx.fillRect(arenaTopLeftX, arenaTopLeftY, arena.width, arena.height);
+	  ctx.fillStyle = "#0000FF";
+	  ctx.fillRect(0, 0, arena.width, arenaTopLeftY);
+	  
+	  ctx.fillStyle = "#00FFFF";
+	  game.players.forEach(function (p) {
+		ctx.fillRect(p.x, p.y, p.size, p.size);
+	  });
+  }
 }
